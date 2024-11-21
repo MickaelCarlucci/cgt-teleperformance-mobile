d5a461b068cc678f5db1d1ea4f5f12476ccf7841
MickaelCarlucci <frewmike17@gmail.com>
2024-11-21 18:19:51 +0100

    add useState for contact page

--- pages/Contact/Contact.jsx
+++ pages/Contact/Contact.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import {s} from "./Contact.style"
 
 
 export const Contact = () => {
+  const [centers, setCenters] = useState([]);
+  const [filteredElected, setFilteredElected] = useState([]);
+  const [rolesData, setRolesData] = useState([]);
+  const [selectedCenter, setSelectedCenter] = useState(null);
+  const [selectedRole, setSelectedRole] = useState(null);
+  const [error, setError] = useState("");
+  
   return (
     <View style={s.container}>
       <Text style={s.text}>Bienvenue sur la page Profile</Text>
